4d05f91b1560a742c1d474b327dbdc61969f3733
Roshan Maddumage <uomroshan@gmail.com>
2024-05-17 20:53:07 +0530

    initial commit

---
+++ app/src/main/java/com/maddumage/composebasiccodelab/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.maddumage.composebasiccodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.maddumage.composebasiccodelab.ui.theme.ComposeBasicCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeBasicCodelabTheme {
+                Scaffold(modifier = Modifier.fillMaxSize(), containerColor = Color.White) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeBasicCodelabTheme {
+        Greeting("Android")
+    }
+}
